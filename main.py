e7e1f194136cedd33c5894afc584b2665a360477
tellusaflame <tellus.aflame@outlook.com>
2023-12-01 12:46:17 +0300

    Reorganized

--- main.py
+++ main.py
@@ -1,11 +1,57 @@
-from models.spark_df import conf_spark, read_jsonl, calc_median, write_json
+import os
+import sys
+import pyspark.sql.functions as f
+from pyspark import SparkConf
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import to_date, col, split
+
+
+def conf_spark() -> SparkSession:
+    """
+    Function sets basic configuration of spark object and relatives
+    """
+
+    # Windows-specific
+    os.environ["PYSPARK_PYTHON"] = sys.executable
+    os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+    spark = (
+        SparkSession.builder.master("local[*]").appName("PySpark_de_ta_5").getOrCreate()
+    )
+    spark.sparkContext.setLogLevel("ERROR")
+
+    conf = SparkConf()
+    conf.set("spark.executor.memory", "4g")
+    conf.set("spark.driver.memory", "4g")
+
+    spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+
+    return spark
+
+
+def calc_median(df: DataFrame) -> DataFrame:
+    """
+    Function calcs median value for supplied df grouped by day and input sensors
+    """
+    df = (
+        df.groupby("date", "input")
+        .agg(f.median("value").alias("median_value"))
+        .sort("date", split(df.input, "_").getItem(1).cast("integer"))
+    )
+    return df
 
 
 def main():
     spark = conf_spark()
-    df = read_jsonl(spark=spark, file_name="data.jsonl")
+
+    df = spark.read.json("data.jsonl")
+
+    df = df.withColumn("date", to_date(col("date"), "yyyy-MM-dd")).drop("time")
+
     df_median = calc_median(df)
-    write_json(df_median)
+
+    (df_median.write.mode("overwrite").json("result"))
+
 
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from models.spark_df import conf_spark, calc_median
+from main import conf_spark, calc_median
 
 spark = conf_spark()
 
@@ -8,28 +8,6 @@ def are_dfs_equal(df1, df2):
 
 
 def test_median_01():
-    input_df = spark.createDataFrame(
-        [
-            ("2020-01-01", "input_01", 1.5),
-            ("2020-01-01", "input_01", 17.0),
-            ("2020-01-01", "input_01", 7.0),
-            ("2020-01-01", "input_01", 3.0),
-            ("2020-01-01", "input_01", 4.0),
-            ("2020-01-01", "input_01", 5.0),
-        ],
-        "date string, input string, value double",
-    )
-    result_df = spark.createDataFrame(
-        [
-            ("2020-01-01", "input_01", 4.5),
-        ],
-        "date string, input string, median_value double",
-    )
-    script_df = calc_median(input_df)
-    assert are_dfs_equal(script_df, result_df) == True
-
-
-def test_median_02():
     input_df = spark.createDataFrame(
         [
             ("2020-01-01", "input_01", 1.5),
